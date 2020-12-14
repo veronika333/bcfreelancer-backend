30f2098f526ac1e2da708841fa266b9655b4dd6b
veronika333 <vera3@inbox.ru>
2020-12-14 18:39:31 +0200

    creating freelancer

--- controllers/freelancer.js
+++ controllers/freelancer.js
@@ -80,25 +80,27 @@ if(files.cv){
     freelancer.cv.data = fs.readFileSync(files.cv.path)
     freelancer.cv.contentType = files.cv.type
 }
-freelancer.save((err, result) => {
-if(err) {
-    return res.status(400).json({
-        message: err
-    })
-}
-res.json(result)
-})
-})
-}
-
-// exports.deleteFreelancer = async (req, res) => {
-//     try{
-//         const removeFreelancer = await Freelancer.remove({_id: req.params.freelancerId});
-//         res.json(removeFreelancer);
-//     } catch(err) {
-//         res.status(204).json({ message: err})
-//     }
+// freelancer.save((err, result) => {
+// if(err) {
+//     return res.status(400).json({
+//         message: err
+//     })
 // }
+// res.json(result)
+// })
+try {
+    freelancer.save();
+    res.status(201).json({
+      freelancer,
+    });
+    console.log("Created a new freelancer", freelancer);
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler(err),
+    })
+    }
+})
+}
 
 exports.deleteFreelancer = (req, res) => {
     const freelancer = req.freelancer
@@ -180,36 +182,6 @@ exports.updateFreelancer = (req, res) => {
         res.status(200).send()
     })
 }
-// exports.updateFreelancer = async (req, res) => {
-//     try{
-//         const updateFreelancer = await Freelancer.findOneAndUpdate({ _id: req.params.freelancerId},
-//         { $set: {
-//             star: req.body.star,
-//             name: req.body.name,
-//             email: req.body.email,
-//             category: req.body.category,
-//             location: req.body.location,
-//             education: req.body.education,
-//             seniority: req.body.seniority,
-//             fi: req.body.fi,
-//             status: req.body.status,
-//             broker: req.body.broker,
-//             company: req.body.company,
-//             date: req.body.date,
-//             contactedBy: req.body.contactedBy,
-//             palkkatoive: req.body.palkkatoive,
-//             huom: req.body.huom,
-//             techRating: req.body.techRating,
-//             cultureFit: req.body.cultureFit,
-//             huonoa: req.body.huonoa,
-//             available: req.body.available,
-//             lineColor: req.body.lineColor
-//         }});
-//         res.json(updateFreelancer);
-//     } catch(err) {
-//         res.status(404).json({ message: err });
-//     }
-// }
 
 // search freelancers
 exports.searchFreelancers = (req, res) => {
